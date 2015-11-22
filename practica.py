e88f23a68b9ee82f38e6fe207dde488dedadbee8
Mayte Remesal <mayteremesal91@gmail.com>
2015-11-22 11:19:14 +0100

    Añadida interfaz HTML simple y posibilidad de consultar distintos números. host cambiado a 0.0.0.0 para ser accesible en red local(ultima linea)

--- practica.py
+++ practica.py
@@ -14,31 +14,30 @@ signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 #FIN código para parar el servidor...
 
-NUMERO_JUGADO = 98190
-
 #definición de ruta raíz.
 @app.route('/')
 def root():
-    return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD. Ejemplo : http://127.0.0.1:5000/2015-11-13'
+    return app.send_static_file('index.html')
 
 #definición de ruta con fecha de consulta
-@app.route('/<fecha>')
-def numero_premiado(fecha):
+@app.route('/<fecha>/<numero_jugado_str>')
+def numero_premiado(fecha, numero_jugado_str):
 	# chequear longitud de fecha correcta
-	if len(fecha) == 10:
-		#intentar leer año mes y dia, si no se puede, hay error de formato
+	if len(fecha) == 10 and len(numero_jugado_str)==5:
+		#intentar leer año mes dia y numero, si no se puede, hay error de formato
 		try:
 			anyo = int(fecha[0:4])
 			mes = int(fecha[5:7])
 			dia = int(fecha[8:10])
+			numero_jugado = int(numero_jugado_str)
 			#chequear logica de fecha (1939 primer sorteo de la ONCE, usado para evitar años absurdos)
 			if mes > 12 or dia > 31 or mes < 1 or dia < 1 or anyo < 1939:
-				return 'Fecha Incorrecta. Por favor especifique la fecha a consultar con formato AAAA-MM-DD. Ejemplo : http://127.0.0.1:5000/2015-11-13'
+				return 'Fecha Incorrecta. Por favor especifique la fecha a consultar con formato AAAA-MM-DD y el número a consultar. Ejemplo : http://127.0.0.1:5000/2015-11-13/98190'
 			fecha_actual = datetime.datetime.now()
 			if anyo > fecha_actual.year or (anyo == fecha_actual.year and mes > fecha_actual.month) or  (anyo == fecha_actual.year and mes == fecha_actual.month and dia >= fecha_actual.day):
-				return 'Fecha futura o actual, sorteo aún no realizado, McFly.'
+				return 'Fecha futura o actual, sorteo aún no realizado'
 		except:
-			return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD. Ejemplo : http://127.0.0.1:5000/2015-11-13'
+			return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD y el número a consultar. Ejemplo : http://127.0.0.1:5000/2015-11-13/98190'
 		#con el año, mes y dia, sacar el dia de la semana (cuponazo el viernes). Si no es posible, hay error de formato
 		if calendar.weekday(anyo, mes, dia) == 0:
 			diadelasemana = "Lunes"
@@ -55,7 +54,7 @@ def numero_premiado(fecha):
 		elif calendar.weekday(anyo, mes, dia) == 6:
 			diadelasemana = "Domingo"
 		else :
-			return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD. Ejemplo : http://127.0.0.1:5000/2015-11-13'
+			return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD y el número a consultar. Ejemplo : http://127.0.0.1:5000/2015-11-13/98190'
 		#hacer una request HTTP GET a la página de resultados, indicando fecha y dia de la semana para que funcione su script.
 		url = 'http://www.resultados11.es/' + diadelasemana +'.php?del-dia=' + str(fecha)
 		# almacenar respuesta en variable
@@ -75,25 +74,25 @@ def numero_premiado(fecha):
 				numero_int = int(numero)
 			except :
 				return 'No hay datos para esta fecha, pruebe otra'	
-			# si lo es compararlo con el número que se juega (definido arriba en NUMERO_JUGADO) y otros premios
-			if numero_int == NUMERO_JUGADO:
-				return "Enhorabuena, el número " + str(NUMERO_JUGADO) + " fue premiado el día " + str(fecha) + " con 35.000 euros"
-			elif numero_int == NUMERO_JUGADO + 1 or numero_int == NUMERO_JUGADO - 1 :
-				return "Enhorabuena, el número " + str(NUMERO_JUGADO) + " fue premiado el día " + str(fecha) + " con 500 euros"
-			elif numero[1:5] == str(NUMERO_JUGADO)[1:5] :
-				return "Enhorabuena, el número " + str(NUMERO_JUGADO) + " fue premiado el día " + str(fecha) + " con 200 euros"
-			elif numero[2:5] == str(NUMERO_JUGADO)[2:5] :
-				return "El número " + str(NUMERO_JUGADO) + " fue premiado el día " + str(fecha) + " con 20 euros"
-			elif numero[0] == str(NUMERO_JUGADO)[0] or numero[4] == str(NUMERO_JUGADO)[4] :
-				return "El número " + str(NUMERO_JUGADO) + " fue premiado el día " + str(fecha) + " con el reintegro (1.50 euros)"
+			# si lo es compararlo con el número que se juega y otros premios
+			if numero_int == numero_jugado:
+				return "Enhorabuena, el número " + str(numero_jugado) + " fue premiado el día " + str(fecha) + " con 35.000 euros"
+			elif numero_int == numero_jugado + 1 or numero_int == numero_jugado - 1 :
+				return "Enhorabuena, el número " + str(numero_jugado) + " fue premiado el día " + str(fecha) + " con 500 euros"
+			elif numero[1:5] == str(numero_jugado)[1:5] :
+				return "Enhorabuena, el número " + str(numero_jugado) + " fue premiado el día " + str(fecha) + " con 200 euros"
+			elif numero[2:5] == str(numero_jugado)[2:5] :
+				return "El número " + str(numero_jugado) + " fue premiado el día " + str(fecha) + " con 20 euros"
+			elif numero[0] == str(numero_jugado)[0] or numero[4] == str(numero_jugado)[4] :
+				return "El número " + str(numero_jugado) + " fue premiado el día " + str(fecha) + " con el reintegro (1.50 euros)"
 			else:
-				return "El número " + str(NUMERO_JUGADO) + " no fue premiado el día " + str(fecha)
+				return "El número " + str(numero_jugado) + " no fue premiado el día " + str(fecha)
 					
 		else :
 			return 'En esta fecha no hubo número premiado o ha ocurrido un error con el servicio de resultados'
 	else :
-		return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD. Ejemplo : http://127.0.0.1:5000/2015-11-13'
+		return 'Por favor especifique la fecha a consultar con formato AAAA-MM-DD y el número a consultar. Ejemplo : http://127.0.0.1:5000/2015-11-13/98190'
 
-#ejecución de app
+#ejecución de app (añadido el host nulo para que se pueda acceder en red, no solo localmente)
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
